4690553dc3e54639ef28113cf88e7659f49c610a
PieroCardoza2003 <pierocardozazapata@gmail.com>
2024-11-17 16:33:02 -0500

    add 17.11.24

--- app/app.py
+++ app/app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import UsuarioRoutes, VehiculoRoutes, PingRoute, EmpresaRoute, ConductorRoute
 
 app = FastAPI()
+
+
+# Lista de orígenes permitidos
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Si estás usando un frontend como React/Vue
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Orígenes permitidos
+    allow_credentials=True,  # Permitir envío de cookies o credenciales
+    allow_methods=["*"],  # Métodos HTTP permitidos (GET, POST, etc.)
+    allow_headers=["*"],  # Encabezados permitidos
+)
 
 app.include_router(PingRoute.router, prefix="/api")
 
